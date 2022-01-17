23fa9fa8bc7e3349465e86c9f68a9b5a60b1e4be
CodeInwader <97461517+CodeInwader@users.noreply.github.com>
2022-01-17 17:53:16 +0100

    Movement

---
+++ Angry Zombies/Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 15f;
+
+    public GameObject hrac;
+    
+
+    public Camera camera;
+
+    public CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mys = Input.mousePosition;
+
+        
+
+        float x = Input.GetAxis("Horizontal");
+         float y = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * y;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
