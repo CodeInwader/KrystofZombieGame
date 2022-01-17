255c5fe6642655aa0ab058a5c5d8803240325016
CodeInwader <97461517+CodeInwader@users.noreply.github.com>
2022-01-17 22:47:54 +0100

    Animations, moving, rotating

--- Angry Zombies/Assets/Player.cs
+++ Angry Zombies/Assets/Player.cs
@@ -7,8 +7,9 @@ public class Player : MonoBehaviour
     public Rigidbody rb;
     public float speed = 15f;
 
-    public GameObject hrac;
-    
+    public Animator Animator;
+
+    public Transform hracTransform;
 
     public Camera camera;
 
@@ -16,15 +17,21 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 mys = Input.mousePosition;
+        Vector3 poziceHraceNaObrazovce = camera.WorldToScreenPoint(hracTransform.position);
+        Vector3 poziceMysiNaObrazovce = Input.mousePosition;
 
-        
+        Vector3 SmerKMysi = poziceMysiNaObrazovce - poziceHraceNaObrazovce;
+        Vector3 FinalniVektor = new Vector3(SmerKMysi.x, 0, SmerKMysi.y);
+
+        hracTransform.rotation = Quaternion.LookRotation(FinalniVektor);
+
+
 
         float x = Input.GetAxis("Horizontal");
          float y = Input.GetAxis("Vertical");
@@ -32,5 +39,15 @@ public class Player : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * y;
 
         controller.Move(move * speed * Time.deltaTime);
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Animator.Play("RunForward");
+
+        }else if (Input.GetKey(KeyCode.S))
+        {
+            Animator.Play("RunBackward");
+        }
     }
 }
