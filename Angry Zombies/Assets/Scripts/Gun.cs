695fb6efb81fb95fffb05df384049a060d66f32e
CodeInwader <97461517+CodeInwader@users.noreply.github.com>
2022-01-18 21:04:17 +0100

    WEEK 1 - COMPLETED
    Ještě se podívat na menší bug v movementu.

---
+++ Angry Zombies/Assets/Scripts/Gun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public Transform gun;
+    public float range = 100f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+
+        if (Physics.Raycast(gun.transform.position, gun.transform.forward * -1, out hit, range))
+        {
+            if (hit.transform.tag == "Zombie")
+            {
+              Zombie.livesOfZombie = Zombie.livesOfZombie - 1;
+                Debug.Log(hit.transform);
+
+            }
+            
+           
+        }
+        
+    }
+}
---
+++ Angry Zombies/Assets/Scripts/Player.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 15f;
+
+    public Animator Animator;
+
+    public int playerLives = 3;
+
+    public Transform hracTransform;
+
+    public Camera camera;
+
+    bool dead = false;
+    bool dieAgain = true;
+
+    public CharacterController controller;
+    
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * y;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+
+        if (dead == false)
+        {
+
+            Vector3 poziceHraceNaObrazovce = camera.WorldToScreenPoint(hracTransform.position);
+            Vector3 poziceMysiNaObrazovce = Input.mousePosition;
+
+            Vector3 SmerKMysi = poziceMysiNaObrazovce - poziceHraceNaObrazovce;
+            Vector3 FinalniVektor = new Vector3(SmerKMysi.x, 0, SmerKMysi.y);
+
+            hracTransform.rotation = Quaternion.LookRotation(FinalniVektor);
+
+          if (Input.GetKey(KeyCode.W))
+          {
+            Animator.Play("RunForward");
+
+          } else if (Input.GetKey(KeyCode.S))
+          {
+            Animator.Play("RunBackward");
+
+          } else if (Input.GetKey(KeyCode.A))
+          {
+            Animator.Play("RunLeft");
+
+          }else if (Input.GetKey(KeyCode.D))
+          {
+            Animator.Play("RunRight");
+
+          }else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+          {
+            Animator.Play("RunForwardLeft");
+
+          }else if (Input.GetKey(KeyCode.W)&& Input.GetKey(KeyCode.D))
+          {
+            Animator.Play("RunForwardRight");
+
+          }else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)) 
+          {
+            Animator.Play("RunBackwardLeft");
+
+          }else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+          {
+            Animator.Play("RunBackwardRight");
+          }
+        
+
+        }
+        
+
+        if (dieAgain == true && playerLives == 0)
+        {
+            dead = true;
+            speed = 0;
+            Animator.Play("Dying");
+            dieAgain = false;
+        }
+        
+
+        
+        
+
+
+        
+    }
+}
